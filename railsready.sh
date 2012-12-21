4c7fce861b205a16f4be3883c933e7b715f1c0e3
brobertsaz <broberts_az@yahoo.com>
2012-12-20 22:04:26 -0700

    added config/unicorn.rb creation

--- railsready.sh
+++ railsready.sh
@@ -196,7 +196,7 @@ fi
 echo "==> done..."
 
 echo -e "\n=> Creating config/unicorn.rb..."
-curl -o config/unicorn.rb https://raw.github.com/defunkt/unicorn/master/examples/unicorn.conf.rb
+curl -O config/unicorn.rb https://raw.github.com/defunkt/unicorn/master/examples/unicorn.conf.rb
 echo "==> done..."
 
 echo -e "\n#################################"
