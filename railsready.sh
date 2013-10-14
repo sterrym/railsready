8b59f61b43424e6e5b0cecc56c8c869bb8a1a66a
Bob Roberts <bob@integrallis.com>
2013-10-13 20:53:21 -0700

    update

--- railsready.sh
+++ railsready.sh
@@ -84,7 +84,7 @@ if [ $isRails -eq 1 ] ; then
   # Where is config at?
   echo -e "\n"
   echo "What is the path to config directory:  "
-  echo  -n ":"
+  echo -n "Path to config?"
   read railsPath
 fi
 
