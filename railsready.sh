5e5992736277a3f6e14e23abed89f93e0ebcc446
Bob Roberts <bob@integrallis.com>
2013-10-13 20:14:41 -0700

    messing with the ruby version

--- railsready.sh
+++ railsready.sh
@@ -11,11 +11,6 @@ pwd#!/bin/bash
 shopt -s nocaseglob
 set -e
 
-ruby_version="2.0.0"
-ruby_version_string="ruby-2.0.0-rc1"
-ruby_source_url="http://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-rc1.tar.gz"
-ruby_source_tar_name="ruby-2.0.0-rc1.tar.gz"
-ruby_source_dir_name="ruby-2.0.0-rc1"
 script_runner=$(whoami)
 railsready_path=$(cd && pwd)/railsready
 log_file="$railsready_path/install.log"
@@ -65,7 +60,7 @@ echo "run tail -f $log_file in a new terminal to watch the install"
 
 echo -e "\n"
 echo "What this script gets you:"
-echo " * Ruby $ruby_version_string"
+echo " * Ruby (your choice of version)"
 echo " * Imagemagick"
 echo " * libs needed to run Rails (sqlite, mysql, etc)"
 echo " * Bundler, Passenger, and Rails gems"
@@ -94,72 +89,90 @@ else
   exit 1
 fi
 
-echo -e "\n=> Creating install dir..."
-cd && mkdir -p railsready/src && cd railsready && touch install.log
-echo "==> done..."
+# Ask you which version of Ruby
+echo -e "\n"
+echo "Select Ruby version"
+echo "=> 1. 1.9.3"
+echo "=> 2. 2.0.0"
+echo -n "Select your Ruby version [1 or 2]? "
+read whichRubyVersion
 
-echo -e "\n=> Downloading and running recipe for $distro...\n"
-#Download the distro specific recipe and run it, passing along all the variables as args
-if [[ $MACHTYPE = *linux* ]] ; then
-  wget --no-check-certificate -O $railsready_path/src/$distro.sh https://raw.github.com/rebel-outpost/railsready/master/recipes/$distro.sh && cd $railsready_path/src && bash $distro.sh $ruby_version $ruby_version_string $ruby_source_url $ruby_source_tar_name $ruby_source_dir_name $whichRuby $railsready_path $log_file
-else
-  cd $railsready_path/src && curl -O https://raw.github.com/rebel-outpost/railsready/master/recipes/$distro.sh && bash $distro.sh $ruby_version $ruby_version_string $ruby_source_url $ruby_source_tar_name $ruby_source_dir_name $whichRuby $railsready_path $log_file
-fi
-echo -e "\n==> done running $distro specific commands..."
+echo $whichRubyVersion
+echo whichRubyVersion
 
-#now that all the distro specific packages are installed lets get Ruby
-if [ $whichRuby -eq 1 ] ; then
-  # Install Ruby
-  echo -e "\n=> Downloading Ruby $ruby_version_string \n"
-  cd $railsready_path/src && wget $ruby_source_url
-  echo -e "\n==> done..."
-  echo -e "\n=> Extracting Ruby $ruby_version_string"
-  tar -xzf $ruby_source_tar_name >> $log_file 2>&1
-  echo "==> done..."
-  echo -e "\n=> Building Ruby $ruby_version_string (this will take a while)..."
-  cd  $ruby_source_dir_name && ./configure --prefix=/usr/local >> $log_file 2>&1 \
-   && make >> $log_file 2>&1 \
-    && sudo make install >> $log_file 2>&1
-  echo "==> done..."
-elif [ $whichRuby -eq 2 ] ; then
-  #thanks wayneeseguin :)
-  echo -e "\n=> Installing RVM the Ruby enVironment Manager http://rvm.beginrescueend.com/rvm/install/ \n"
-  curl -O -L -k https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer
-  chmod +x rvm-installer
-  "$PWD/rvm-installer" >> $log_file 2>&1
-  [[ -f rvm-installer ]] && rm -f rvm-installer
-  echo -e "\n=> Setting up RVM to load with new shells..."
-  #if RVM is installed as user root it goes to /usr/local/rvm/ not ~/.rvm
-  if [ -f ~/.bashrc ] ; then
-    echo  '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"  # Load RVM into a shell session *as a function*' >> "$HOME/.bashrc"
-  fi
-  if [ -f ~/.bash_profile ] ; then
-    echo  '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"  # Load RVM into a shell session *as a function*' >> "$HOME/.bash_profile"
-  fi
-  echo "==> done..."
-  echo "=> Loading RVM..."
-  if [ -f ~/.bashrc ] ; then
-    source ~/.bashrc
-  fi
-  if [ -f ~/.bash_profile ] ; then
-    source ~/.bash_profile
-  fi
-  if [ -f ~/.rvm/scripts/rvm ] ; then
-    source ~/.rvm/scripts/rvm
-  fi
-  echo "==> done..."
-  echo -e "\n=> Installing Ruby $ruby_version_string (this will take a while)..."
-  echo -e "=> More information about installing rubies can be found at http://rvm.beginrescueend.com/rubies/installing/ \n"
-  rvm install $ruby_version >> $log_file 2>&1
-  echo -e "\n==> done..."
-  echo -e "\n=> Using $ruby_version and setting it as default for new shells..."
-  echo "=> More information about Rubies can be found at http://rvm.beginrescueend.com/rubies/default/"
-  rvm --default use $ruby_version >> $log_file 2>&1
-  echo "==> done..."
-else
-  echo "How did you even get here?"
-  exit 1
-fi
+# if [ $whichRubyVersion -eq 1 ] ; then
+# ruby_version="2.0.0"
+# ruby_version_string="ruby-2.0.0"
+# ruby_source_url="http://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0.tar.gz"
+# ruby_source_tar_name="ruby-2.0.0.tar.gz"
+# ruby_source_dir_name="ruby-2.0.0"
+
+# echo -e "\n=> Creating install dir..."
+# cd && mkdir -p railsready/src && cd railsready && touch install.log
+# echo "==> done..."
+
+# echo -e "\n=> Downloading and running recipe for $distro...\n"
+# #Download the distro specific recipe and run it, passing along all the variables as args
+# if [[ $MACHTYPE = *linux* ]] ; then
+#   wget --no-check-certificate -O $railsready_path/src/$distro.sh https://raw.github.com/rebel-outpost/railsready/master/recipes/$distro.sh && cd $railsready_path/src && bash $distro.sh $ruby_version $ruby_version_string $ruby_source_url $ruby_source_tar_name $ruby_source_dir_name $whichRuby $railsready_path $log_file
+# else
+#   cd $railsready_path/src && curl -O https://raw.github.com/rebel-outpost/railsready/master/recipes/$distro.sh && bash $distro.sh $ruby_version $ruby_version_string $ruby_source_url $ruby_source_tar_name $ruby_source_dir_name $whichRuby $railsready_path $log_file
+# fi
+# echo -e "\n==> done running $distro specific commands..."
+
+# #now that all the distro specific packages are installed lets get Ruby
+# if [ $whichRuby -eq 1 ] ; then
+#   # Install Ruby
+#   echo -e "\n=> Downloading Ruby $ruby_version_string \n"
+#   cd $railsready_path/src && wget $ruby_source_url
+#   echo -e "\n==> done..."
+#   echo -e "\n=> Extracting Ruby $ruby_version_string"
+#   tar -xzf $ruby_source_tar_name >> $log_file 2>&1
+#   echo "==> done..."
+#   echo -e "\n=> Building Ruby $ruby_version_string (this will take a while)..."
+#   cd  $ruby_source_dir_name && ./configure --prefix=/usr/local >> $log_file 2>&1 \
+#    && make >> $log_file 2>&1 \
+#     && sudo make install >> $log_file 2>&1
+#   echo "==> done..."
+# elif [ $whichRuby -eq 2 ] ; then
+#   #thanks wayneeseguin :)
+#   echo -e "\n=> Installing RVM the Ruby enVironment Manager http://rvm.beginrescueend.com/rvm/install/ \n"
+#   curl -O -L -k https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer
+#   chmod +x rvm-installer
+#   "$PWD/rvm-installer" >> $log_file 2>&1
+#   [[ -f rvm-installer ]] && rm -f rvm-installer
+#   echo -e "\n=> Setting up RVM to load with new shells..."
+#   #if RVM is installed as user root it goes to /usr/local/rvm/ not ~/.rvm
+#   if [ -f ~/.bashrc ] ; then
+#     echo  '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"  # Load RVM into a shell session *as a function*' >> "$HOME/.bashrc"
+#   fi
+#   if [ -f ~/.bash_profile ] ; then
+#     echo  '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"  # Load RVM into a shell session *as a function*' >> "$HOME/.bash_profile"
+#   fi
+#   echo "==> done..."
+#   echo "=> Loading RVM..."
+#   if [ -f ~/.bashrc ] ; then
+#     source ~/.bashrc
+#   fi
+#   if [ -f ~/.bash_profile ] ; then
+#     source ~/.bash_profile
+#   fi
+#   if [ -f ~/.rvm/scripts/rvm ] ; then
+#     source ~/.rvm/scripts/rvm
+#   fi
+#   echo "==> done..."
+#   echo -e "\n=> Installing Ruby $ruby_version_string (this will take a while)..."
+#   echo -e "=> More information about installing rubies can be found at http://rvm.beginrescueend.com/rubies/installing/ \n"
+#   rvm install $ruby_version >> $log_file 2>&1
+#   echo -e "\n==> done..."
+#   echo -e "\n=> Using $ruby_version and setting it as default for new shells..."
+#   echo "=> More information about Rubies can be found at http://rvm.beginrescueend.com/rubies/default/"
+#   rvm --default use $ruby_version >> $log_file 2>&1
+#   echo "==> done..."
+# else
+#   echo "How did you even get here?"
+#   exit 1
+# fi
 
 # Reload bash
 echo -e "\n=> Reloading shell so ruby and rubygems are available..."
