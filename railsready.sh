4b40ae4c771c61167d1cc828c55736eed83078de
Bob Roberts <bob@integrallis.com>
2013-10-16 20:23:45 -0700

    got the params set to pass thru

--- railsready.sh
+++ railsready.sh
@@ -138,7 +138,7 @@ fi
 echo -e "\n=> Downloading and running recipe for $distro...\n"
 #Download the distro specific recipe and run it, passing along all the variables as args
 if [[ $MACHTYPE = *linux* ]] ; then
-  wget --no-check-certificate -O $railsready_path/src/$distro.sh https://raw.github.com/rebel-outpost/railsready/master/recipes/$distro.sh && cd $railsready_path/src && bash $distro.sh $ruby_version $ruby_version_string $ruby_source_url $ruby_source_tar_name $ruby_source_dir_name $whichRuby $railsready_path $log_file
+  wget --no-check-certificate -O $railsready_path/src/$distro.sh https://raw.github.com/rebel-outpost/railsready/master/recipes/$distro.sh && cd $railsready_path/src && bash $distro.sh $ruby_version $ruby_version_string $ruby_source_url $ruby_source_tar_name $ruby_source_dir_name $whichRuby $whichServer $whichDatabase $railsready_path $log_file
 else
   cd $railsready_path/src && curl -O https://raw.github.com/rebel-outpost/railsready/master/recipes/$distro.sh && bash $distro.sh $ruby_version $ruby_version_string $ruby_source_url $ruby_source_tar_name $ruby_source_dir_name $whichRuby $railsready_path $log_file
 fi
--- recipes/ubuntu.sh
+++ recipes/ubuntu.sh
@@ -15,11 +15,12 @@ ruby_source_url=$3
 ruby_source_tar_name=$4
 ruby_source_dir_name=$5
 whichRuby=$6 # 1=source 2=RVM
+script_runner=$(whoami)
 whichServer=$9
 whichDatabase=$10
-script_runner=$(whoami)
 railsready_path=$7
 log_file=$8
+
 
 # echo "vars set: $ruby_version $ruby_version_string $ruby_source_url $ruby_source_tar_name $ruby_source_dir_name $whichRuby $whichServer $whichDatabase $railsready_path $log_file"
 echo "\nServer == $whichServer"
