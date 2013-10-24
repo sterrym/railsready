682f55e4dc88eb1172e91122ad7345bd294dc809
Tim Glen <tim@tag.ca>
2013-10-24 10:03:10 -0400

    have to read the env back in to make rbenv runnable

--- railsready.sh
+++ railsready.sh
@@ -138,6 +138,7 @@ elif [ $whichRuby -eq 2 ] ; then
   git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
   echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
   echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+  source ~/.bash_profile
   echo -e "\n=> Installing ruby-build  \n"
   git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
   echo -e "\n=> Installing ruby \n"
